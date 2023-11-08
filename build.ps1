0a991cab61724865bb289a565b9d25e68e17bc0e
Yushun Cheng <chengys@disroot.org>
2023-11-08 11:31:31 +0800

    Remove build scripts.

--- build.ps1
+++
@@ -1,3 +0,0 @@
-cargo build --release
-remove-item bin/cpc.exe
-move-item -path target/release/CAIE_Code.exe -destination bin/cpc.exe
--- build.sh
+++
@@ -1,3 +0,0 @@
-rm -rf target
-cargo build --release
-strip -o bin/cpc target/release/CAIE_Code
